ebd07d480a1dbfbb26b81ccd6212c67451cdebef
roddeacon <rod.t.deacon@gmail.com>
2023-07-03 16:11:28 -0700

    added a function to find available terminals

--- nidaqmx_python_testbed/voltage_test.py
+++ nidaqmx_python_testbed/voltage_test.py
@@ -1,7 +1,7 @@
 ''' 
 Rodney Deacon
 6/29/2023
-University of Washington Fu Lab PLE code testground
+Fu Lab PLE code testground
 
 system info from : https://github.com/ni/nidaqmx-python/blob/master/examples/system_properties.py
 
@@ -10,12 +10,11 @@ output voltage test from : https://github.com/ni/nidaqmx-python/blob/master/exam
 import nidaqmx
 import traceback # temp for testing
 
-devices_found = False # used for checking if there are devices found DONT CHANGE
+# global for testing if a device found (currently depreciated keep true)
+devices_found = True
 
 def get_system_info():
-    """ used to get local system information and see if there is a successful communication with DAQ.
-    """
-    
+# test if there is successful communication with the DAQ, try and print system information
     local_system = nidaqmx.system.system.System.local() #create system object to get system info
     driver_version = local_system.driver_version # assign local_system's driver_version property to a driver_version variable
 
@@ -46,31 +45,29 @@ def get_system_info():
 
 # the following context manager is testing output voltage 
 def test_out_voltage():
-    """ used for testing output voltage
-    #TODO: find more elegant failsafe
-    
-    """
-    
-    # temporary failsafe if Dev1 is not detected and displayed in terminal
-    #get user input
-    choice = input("please enter 1 to continue or anything else to quit.\n--> ")
+    with nidaqmx.Task() as myTask: # instantiate a new task called "myTask"
+            myTask.ao_channels.add_ao_voltage_chan("Dev1/ao3") # add a new voltage channel to the task
 
-#TODO: no choice for anything other than 0 or 1
-    if choice == "1":
-        with nidaqmx.Task() as myTask: # instantiate a new task called "myTask"
-                myTask.ao_channels.add_ao_voltage_chan("Dev1/ao3") # add a new voltage channel to the task
+            myTask.timing.cfg_samp_clk_timing(2000) # ASSUMED TO BE MILISECONDS
 
-                myTask.timing.cfg_samp_clk_timing(2000) # ASSUMED TO BE MILISECONDS
+            print(myTask.write(3, auto_start=True)) # "writes" 3 , to Dev1/ao3 for 1 clock timing
 
-                print(myTask.write(3, auto_start=True)) # "writes" 3 , to Dev1/ao3 for 1 clock timing
+            myTask.wait_until_done() # waits for task is complete
+            myTask.stop()# stops task
 
-                myTask.wait_until_done() # waits for task is complete
-                myTask.stop()# stops task
+def available_terminals():
+    '''loop through available terminals and print them out
+    '''
+    device = nidaqmx.system.device.Device("Dev1")
+    for tr in device.terminals:
+	    print(tr)
 
-    
-         
+# main program areaa
 print("\ndriver version:")
 get_system_info()
+
+print("\navailable terminals:\n")
+available_terminals()
 
 print("\nattempting to generate test volatage on \"Dev1\\ao3\"..")
 if devices_found: # if there are devices, try to run the function test_out_voltage() #TODO: device specific statements or menu    
